d32816ebbc2b3289de9bac15ab95987664e51798
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2023-12-13 13:27:51 +0200

    Added unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -18,4 +20,20 @@ const favoriteBlog = blogs => {
     return blogs.find(blog => blog.likes === Math.max(...likes))
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = blogs => {
+    const authors = blogs.map(blog => blog.author)
+    const blogsPerAuthor = _.countBy(authors, (x) => x)
+    const authorWithMostBlogs = _.maxBy(_.entries(blogsPerAuthor), ([k, v]) => v)
+    const result = { author: authorWithMostBlogs[0], blogs: authorWithMostBlogs[1] }
+    return result
+}
+
+const mostLikes = blogs => {
+    const result = _(blogs).groupBy('author').map((objs, key) => ({
+        'author': key,
+        'likes': _.sumBy(objs, 'likes')
+    })).maxBy('likes')
+    return result
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
