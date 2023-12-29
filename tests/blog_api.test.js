501be721e9a90aec314b9065b9b8190655268b70
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2023-12-29 13:21:18 +0200

    Done with 4.23

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,103 +14,184 @@ beforeEach(async () => {
 })
 
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-}, 10000)
+describe('viewing blogs', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    }, 10000)
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('blogs have id instead of _id', async () => {
+        const res = await api.get('/api/blogs')
+        expect(res.body[0].id).toBeDefined()
+    })
 })
 
-test('blogs have id instead of _id', async () => {
-    const res = await api.get('/api/blogs')
-    expect(res.body[0].id).toBeDefined()
+describe('creating blogs', () => {
+    test('making post request successfully creates a new blog', async () => {
+        const loginInfo = {
+            username: "root",
+            password: "bigsekret"
+        }
+
+        const newBlog = {
+            author: "Coding legend",
+            title: "Never use console.log",
+            url: "www.verigudcode.net",
+            likes: "1337",
+        }
+
+        const user = await api
+            .post('/api/login')
+            .send(loginInfo)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .set('Authorization', `Bearer ${user.body.token}`)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+        const authors = blogsAtEnd.map(b => b.author)
+        expect(authors).toContain('Coding legend')
+    })
+
+    test('if likes missing, defaults to 0', async () => {
+        const loginInfo = {
+            username: "root",
+            password: "bigsekret"
+        }
+
+        const newBlog = {
+            author: "Coding legend",
+            title: "Never use console.log",
+            url: "www.verigudcode.net",
+        }
+
+        const user = await api
+            .post('/api/login')
+            .send(loginInfo)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .set('Authorization', `Bearer ${user.body.token}`)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        const titles = blogsAtEnd.map(b => b.title)
+        expect(titles).toContain(newBlog.title)
+    })
+
+    test('does not accept missing title or url', async () => {
+        const newBlog = {
+            author: "Not gonna happen",
+            likes: 5
+        }
+        const loginInfo = {
+            username: "root",
+            password: "bigsekret"
+        }
+
+        const user = await api
+            .post('/api/login')
+            .send(loginInfo)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .set('Authorization', `Bearer ${user.body.token}`)
+            .expect(400)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('fails with 401 if token not provided', async () => {
+        const newBlog = {
+            title: "You don't need authentication to post blogs",
+            author: "Unauthenticated",
+            url: "www.freeworld.org",
+            likes: 69
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(401)
+    })
 })
 
-test('making post request successfully creates a new blog', async () => {
-    const newBlog = {
-        author: "Coding legend",
-        title: "Never use console.log",
-        url: "verygudcod.net",
-        likes: "1337",
-    }
+describe('deleting blogs', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+        const loginInfo = {
+            username: "root",
+            password: "bigsekret"
+        }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const newBlog = {
+            author: "Coding legend",
+            title: "Never use console.log",
+            url: "www.verigudcode.net",
+            likes: "1337",
+        }
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        const user = await api
+            .post('/api/login')
+            .send(loginInfo)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
-    const authors = blogsAtEnd.map(b => b.author)
-    expect(authors).toContain('Coding legend')
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .set('Authorization', `Bearer ${user.body.token}`)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtStart = await helper.blogsInDb()
+
+        await api
+            .delete(`/api/blogs/${blogsAtStart[blogsAtStart.length - 1].id}`)
+            .set('Authorization', `Bearer ${user.body.token}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        const titles = blogsAtEnd.map(b => b.title)
+        expect(titles).not.toContain(newBlog.title)
+    })
 })
 
-test('if likes missing, defaults to 0', async () => {
-    const newBlog = {
-        author: "One wise guy",
-        title: "Aliens are real",
-        url: "meetaliens.net",
-    }
+describe('updating blogs', () => {
+    test('valid update succeeds', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const updatedBlog = { ...blogsAtStart[0], likes: 69 }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        await api
+            .put(`/api/blogs/${updatedBlog.id}`)
+            .send(updatedBlog)
+            .expect(200)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    const titles = blogsAtEnd.map(b => b.title)
-    expect(titles).toContain(newBlog.title)
-})
-
-test('does not accept missing title or url', async () => {
-    const newBlog = {
-        author: "Not gonna happen",
-        likes: 5
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('succeeds with status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-
-    await api
-        .delete(`/api/blogs/${blogsAtStart[0].id}`)
-        .expect(204)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
-
-    const titles = blogsAtEnd.map(b => b.title)
-    expect(titles).not.toContain(blogsAtStart[0].title)
-})
-
-test('valid update succeeds', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const updatedBlog = { ...blogsAtStart[0], likes: 69 }
-
-    await api
-        .put(`/api/blogs/${updatedBlog.id}`)
-        .send(updatedBlog)
-        .expect(200)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toContainEqual(updatedBlog)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toContainEqual(updatedBlog)
+    })
 })
 
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -19,6 +19,9 @@ const tokenExtractor = (request, response, next) => {
 	if (authorization && authorization.startsWith('Bearer ')) {
 		request.token = authorization.replace('Bearer ', '')
 	}
+	else {
+		response.status(401).json({ error: 'Unauthorized: missing token' })
+	}
 	next()
 }
 
@@ -27,7 +30,6 @@ const userExtractor = async (request, response, next) => {
 		const decodedToken = jwt.verify(request.token, process.env.SECRET)
 		if (decodedToken.id) {
 			request.user = await User.findById(decodedToken.id)
-			console.log(request.user)
 		}
 		else {
 			response.status(401).json({ error: 'invalid token' })
@@ -43,6 +45,7 @@ const errorHandler = (error, request, response, next) => {
 	if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' })
 	else if (error.name === 'ValidationError') return response.status(400).json({ error: error.message })
 	else if (error.name === 'JsonWebTokenError') return response.status(401).json({ error: error.message })
+	else if (error.name === 'AuthorizationError')
 
 	next(error)
 }
